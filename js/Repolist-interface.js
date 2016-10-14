f7b5444d8afaa30ec08deb21570081b62e1521e9
Ben Vissotzky <ben.vissotzky@gmail.com>
2016-10-14 09:26:42 -0700

    add form input, make api call to collect github userfrom given username

--- js/Repolist-interface.js
+++ js/Repolist-interface.js
@@ -1,3 +1,12 @@
+var getRepos = require('./../js/Repolist.js').getRepos;
+
 $(document).ready(function() {
-  
+
+
+  $('#user-submit').submit(function(event) {
+    event.preventDefault();
+    var username = $('#username').val();
+    getRepos(username);
+
+  });
 });
